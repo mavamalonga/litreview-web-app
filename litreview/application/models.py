510f002964e0fc421ff96b90a63c87ad4a9fcbce
Alpha <mavamalonga.alpha@gmail.com>
2021-12-05 21:36:47 +0100

    template sign-in and sign-up

--- litreview/application/models.py
+++ litreview/application/models.py
@@ -25,3 +25,4 @@ class Review(models.Model):
 	headline = models.CharField(max_length=128)
 	body = models.TextField(max_length=8192, blank=True)
 	time_created = models.DateTimeField(auto_now_add=True)
+
--- litreview/authentication/forms.py
+++ litreview/authentication/forms.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 class SignUpForm(UserCreationForm):
 	class Meta(UserCreationForm):
 		model = get_user_model()
-		fields = ('username','email', 'first_name', 'last_name')
+		fields = ('username','email')
 			
 
 class LoginForm(forms.Form):
