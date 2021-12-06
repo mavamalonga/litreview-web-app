03a3ca3b1d53eb35bb47dddd783a2380bae62849
Alpha <mavamalonga.alpha@gmail.com>
2021-12-06 12:50:32 +0100

    add header style

--- litreview/application/models.py
+++ litreview/application/models.py
@@ -10,7 +10,7 @@ class Photo(models.Model):
 class Ticket(models.Model):
 	photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
 	title = models.CharField(max_length=128)
-	content = models.CharField(max_length=5000)
+	content = models.TextField(max_length=5000)
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	date_created = models.DateTimeField(auto_now_add=True)
 
